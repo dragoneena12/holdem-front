c3eb52db68cca7ca40408d184102936974e14efb
lapi <tyoutugai.7@gmail.com>
2020-12-18 02:16:56 +0900

    fix models

--- models/table.ts
+++ models/table.ts
@@ -7,16 +7,22 @@ export class Table implements Itable {
     name: string;
     bankroll: number;
   } | null)[] = [];
+  buttonPlayer: number | null = null;
+  currentPlayer: number | null = null;
 
   setTableStatus(
     state: string,
-    seatingChart: ({
+    seatingChart?: ({
       id: string;
       name: string;
       bankroll: number;
-    } | null)[]
+    } | null)[],
+    buttonPlayer?: number,
+    currentPlayer?: number
   ): void {
     this.state = state;
-    this.seatingChart = seatingChart;
+    this.seatingChart = seatingChart ? seatingChart : this.seatingChart;
+    this.buttonPlayer = buttonPlayer ? buttonPlayer : this.buttonPlayer;
+    this.currentPlayer = currentPlayer ? currentPlayer : this.currentPlayer;
   }
 }
